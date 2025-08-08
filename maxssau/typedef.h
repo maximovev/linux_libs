71f2722e8f95800eac35ed707ca036f486039e95
maximovev <131649268+maximovev@users.noreply.github.com>
2025-08-08 16:39:14 +0400

    Update typedef.h

--- maxssau/typedef.h
+++ maxssau/typedef.h
@@ -1,10 +1,11 @@
-
 #ifndef __typeconvert__
 #define __typeconvert__
 
 /*
 Maximov Evgeny
 Russia, Samara, 13 may 2025
+
+upd: 2025/08/06 - add align and no align
 
 Data-to-bytes converter
 */
@@ -14,14 +15,21 @@ namespace maxssau
 
 #pragma pack(push, 1)
 
-template<typename StructType> union byte_converter
+template<typename StructType> union byte_converter_no_align
 {
     public:
         StructType value;
-        unsigned char bytes[sizeof(StructType)];    
+        unsigned char bytes[sizeof(StructType)];
 };
 
 #pragma pack(pop)
+
+template<typename StructType> union byte_converter_align
+{
+    public:
+        StructType value;
+        unsigned char bytes[sizeof(StructType)];
+};
 
 }
 
